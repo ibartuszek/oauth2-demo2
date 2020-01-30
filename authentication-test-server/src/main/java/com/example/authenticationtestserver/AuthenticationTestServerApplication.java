6fc87ff1acead5230b78e6b39c76776f1eb2dffb
Istvan Bartuszek <v-ibartuszek@hotels.com>
2020-01-30 07:45:09 +0100

    First commit

---
+++ authentication-test-server/src/main/java/com/example/authenticationtestserver/AuthenticationTestServerApplication.java
@@ -0,0 +1,13 @@
+package com.example.authenticationtestserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuthenticationTestServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AuthenticationTestServerApplication.class, args);
+	}
+
+}
