9eb32fd5a6d46bde7ac477886dafd70604ab1c4d
ibartuszek <istvan.bartuszek@gmail.com>
2020-04-06 21:23:30 +0200

    Create authorization server with tokenstore

--- spring-authorization-test-server/src/test/java/com/example/authorizationserver/AuthorizationServerTestApplicationTests.java
+++ spring-authorization-test-server/src/test/java/com/example/authorizationserver/AuthorizationServerTestApplicationTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class AuthorizationTestServerApplicationTests {
+class AuthorizationServerTestApplicationTests {
 
 	@Test
 	void contextLoads() {
