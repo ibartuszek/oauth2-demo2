e4d0d568e0160f303adfa1b23a422d659e0d08eb
ibartuszek <istvan.bartuszek@gmail.com>
2020-04-07 13:02:45 +0200

    Add security to resource server

--- resource-test-server/src/main/java/com/example/resourcetestserver/web/ExampleResourceController.java
+++ resource-test-server/src/main/java/com/example/resourcetestserver/web/ExampleResourceController.java
@@ -2,6 +2,7 @@ package com.example.resourcetestserver.web;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -16,12 +17,22 @@ public class ExampleResourceController {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(ExampleResourceController.class);
 
-    @RequestMapping(value = "oauth2example/resource/example", method = RequestMethod.GET, produces = APPLICATION_JSON)
-    public ExampleResource getTestResponse() {
-        LOGGER.info("Resource server returns: \'" + TEST_RESPONSE + "\' test response");
+    @PreAuthorize("hasRole('ROLE_USER')")
+    @RequestMapping(value = "oauth2example/resource/exampleForUsers", method = RequestMethod.GET, produces = APPLICATION_JSON)
+    public ExampleResource getTestResponseForUsers() {
+        LOGGER.info("Resource server returns: \'" + TEST_RESPONSE + "\' test response (for users)");
         return ExampleResource.builder()
                 .withMessage(TEST_RESPONSE)
                 .build();
+    }
+
+    @PreAuthorize("hasRole('ROLE_CLIENT')")
+    @RequestMapping(value = "oauth2example/resource/exampleForClients", method = RequestMethod.GET, produces = APPLICATION_JSON)
+    public ExampleResource getTestResponseForClients() {
+        LOGGER.info("Resource server returns: \'" + TEST_RESPONSE + "\' test response (for clients)");
+        return ExampleResource.builder()
+            .withMessage(TEST_RESPONSE)
+            .build();
     }
 
 }
